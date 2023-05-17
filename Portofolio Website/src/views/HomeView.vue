4637729de2e1f3e5ac5b4fe783e819a78f3c8cbf
Max <maxnouts@gmail.com>
2023-05-17 13:45:07 +0200

    Made leeruitkomsten pagina.

--- Portofolio Website/src/views/HomeView.vue
+++ Portofolio Website/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/Carousel.vue'
+import Carousel from '../components/Carousel.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Carousel />
   </main>
 </template>
