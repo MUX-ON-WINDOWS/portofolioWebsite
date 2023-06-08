a01bd4ceb910c070b6d7bc91e8959e7b16cf7b18
Max <maxnouts@gmail.com>
2023-06-08 17:38:23 +0200

    added spacing around leerdoelen

--- Portofolio Website/src/views/AboutView.vue
+++ Portofolio Website/src/views/AboutView.vue
@@ -2,43 +2,47 @@
   <div class="container_col">
     <!-- rij 1 -->
     <div class="container_row">
-    <div class="leerdoel1">
-      <h1>INTERACTIVE MEDIA</h1>
-    </div>
-    <div class="border1"></div>
-    <div class="container_content_uitleg1">
-      <h3>Reflectie:</h3>
-        Mijn reflectie is hoe meer versies je maakt hoe meer mogelijke heden hebt tot een goed eind ontwerp.
-        De website designs heb ik gemaakt in Figma en de kaarten heb ik gemaakt in illustrator.
-        Zelf vind ik dat het website design van Kathleen niet echt heel goed uit de verf is gekomen.
-        De kaarten die ik heb gemaakt zijn wel zeer goed gelukt.
+      <div class="leerdoel1">
+        <h1>INTERACTIVE MEDIA</h1>
+      </div>
+      <div class="border1"></div>
+      <div class="container_content_uitleg1">
+        <div class="content_container_margin">
+            <h3>Reflectie:</h3>
+              Mijn reflectie is hoe meer versies je maakt hoe meer mogelijke heden hebt tot een goed eind ontwerp.
+              De website designs heb ik gemaakt in Figma en de kaarten heb ik gemaakt in illustrator.
+              Zelf vind ik dat het website design van Kathleen niet echt heel goed uit de verf is gekomen.
+              De kaarten die ik heb gemaakt zijn wel zeer goed gelukt.
 
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://www.figma.com/file/t62ZqG0sSBJiLgyNA3QW5O/Katleen-Website?node-id=0%3A1&t=2FSAV7a3mpk1iuD1-1">Website design Kathleen.</a></li>
-        <li><a href="https://www.figma.com/file/98FJ9ml8PfQ3Ie24TaC50l/Untitled?node-id=0%3A1&t=cz8sJ2BzIGLVa301-1">Website design Eigen project.</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/Mac campaign/landen/Belgie/Dries Mertens.png">Kaarten landen</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/Mac campaign/MacTeam.ai">Speciaal Macdonalds kaarten</a></li>
-      </ul>
-    </div>
+            <h3>Bewijs:</h3>
+            <ul>
+              <li><a href="https://www.figma.com/file/t62ZqG0sSBJiLgyNA3QW5O/Katleen-Website?node-id=0%3A1&t=2FSAV7a3mpk1iuD1-1">Website design Kathleen.</a></li>
+              <li><a href="https://www.figma.com/file/98FJ9ml8PfQ3Ie24TaC50l/Untitled?node-id=0%3A1&t=cz8sJ2BzIGLVa301-1">Website design Eigen project.</a></li>
+              <li><a href="https://i507258.hera.fhict.nl/Mac campaign/landen/Belgie/Dries Mertens.png">Kaarten landen</a></li>
+              <li><a href="https://i507258.hera.fhict.nl/Mac campaign/MacTeam.ai">Speciaal Macdonalds kaarten</a></li>
+            </ul>
+          </div>
+      </div>
   </div>
   <!-- rij 2 -->
   <div class="container_row">
-    <div class="container_content_uitleg2" style="height: 280px;">
-      <h3>Reflectie:</h3>
-      Mijn reflectie is hoe beter je je werk uitwerkt (uitschrijven wat je wilt) voordat je begint met de
-      development hoe makkelijker het is om het te realiseren. Wat de volgende keer beter kan is het beter door lezen van documentatie.
-      Bv bij de nachtegaal Vue.js project setup ging het al fout, doordat ik geen routers had geven.
-      Hierdoor kon ik geen pagina’s linken. Het maken van nieuwe dingen in JS zelf ging zeer goed.
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://github.com/MUX-ON-WINDOWS/Zangschool-De-Nachtegaal-V2.git">Zangvogel</a></li>
-        <li><a href="https://codepen.io/mux-on-windows/pen/KKxajqY">Css opdracht</a></li>
-        <li><a href="https://git.fhict.nl/I507258/bingojavascript">Bingo kaart generator</a></li>
-        <li><a href="https://github.com/MUX-ON-WINDOWS/webbrowserGameJS">Mini game</a></li>
-        <li><a href="https://git.fhict.nl/I509147/project-mccampaign">PenaltyShooter</a></li>
-        <li><a href="https://github.com/MUX-ON-WINDOWS/KeeperWebshop.Vue">Keeperwebshop (eigen project)</a></li>
-      </ul>
+    <div class="container_content_uitleg2">
+      <div class="content_container_margin">
+        <h3>Reflectie:</h3>
+        Mijn reflectie is hoe beter je je werk uitwerkt (uitschrijven wat je wilt) voordat je begint met de
+        development hoe makkelijker het is om het te realiseren. Wat de volgende keer beter kan is het beter door lezen van documentatie.
+        Bv bij de nachtegaal Vue.js project setup ging het al fout, doordat ik geen routers had geven.
+        Hierdoor kon ik geen pagina’s linken. Het maken van nieuwe dingen in JS zelf ging zeer goed.
+        <h3>Bewijs:</h3>
+        <ul>
+          <li><a href="https://github.com/MUX-ON-WINDOWS/Zangschool-De-Nachtegaal-V2.git">Zangvogel</a></li>
+          <li><a href="https://codepen.io/mux-on-windows/pen/KKxajqY">Css opdracht</a></li>
+          <li><a href="https://git.fhict.nl/I507258/bingojavascript">Bingo kaart generator</a></li>
+          <li><a href="https://github.com/MUX-ON-WINDOWS/webbrowserGameJS">Mini game</a></li>
+          <li><a href="https://git.fhict.nl/I509147/project-mccampaign">PenaltyShooter</a></li>
+          <li><a href="https://github.com/MUX-ON-WINDOWS/KeeperWebshop.Vue">Keeperwebshop (eigen project)</a></li>
+        </ul>
+      </div>
     </div>
     <div class="border2"></div>
     <div class="leerdoel2">
@@ -52,32 +56,36 @@
     </div>
     <div class="border1"></div>
     <div class="container_content_uitleg1">
-      <h3>Reflectie:</h3>
-        Veel feedback gevraagd voor het maken van de kaarten. Hier kwam uit dat ik meet het Mac Donalds element moest toe passen
-        denk aan de kleuren. Voor Kathleen was zeer duidelijk dat het een Instagram pagina was het font kwam alleen niet over
-        heen met die van Instagram zelf.
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://i507258.hera.fhict.nl/Kathleen/KathleenWillemsInsta_Max.png">Instagram pagina</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/Mac campaign/landen/Belgie/Dries Mertens.png">Voetbal kaarten</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/Mac campaign/MacTeam.ai">Speciale kaarten (met patroon)</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/KeepersWebShop/Logo/goalKeeperLogo_MaxV2.png">Logo</a></li>
-      </ul>
+      <div class="content_container_margin">
+        <h3>Reflectie:</h3>
+          Veel feedback gevraagd voor het maken van de kaarten. Hier kwam uit dat ik meet het Mac Donalds element moest toe passen
+          denk aan de kleuren. Voor Kathleen was zeer duidelijk dat het een Instagram pagina was het font kwam alleen niet over
+          heen met die van Instagram zelf.
+        <h3>Bewijs:</h3>
+        <ul>
+          <li><a href="https://i507258.hera.fhict.nl/Kathleen/KathleenWillemsInsta_Max.png">Instagram pagina</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/Mac campaign/landen/Belgie/Dries Mertens.png">Voetbal kaarten</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/Mac campaign/MacTeam.ai">Speciale kaarten (met patroon)</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/KeepersWebShop/Logo/goalKeeperLogo_MaxV2.png">Logo</a></li>
+        </ul>
+      </div>
     </div>
   </div>
   <!-- rij 4 -->
   <div class="container_row">
     <div class="container_content_uitleg2">
-      <h3>Reflectie:</h3>
-      Ik ben tot conclusie gekomen dat het best lastig kan zijn om sommige dingen te onderzoeken waar niet al te
-      veel data over bekent is. En als je data hebt gevonden moet je dit altijd goed controleren zodat je geen foute data gebruikt.
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://i507258.hera.fhict.nl/Kathleen/onderzoekPlanning.docx">Post tijden TikTok & Instagram</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/research/Penalty Shooter research.docx">Penaltyshooter research</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/research/Onderzoek framework eigen project.docx">Onderzoek framework eigen project</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/research/Onderzoek prijs variantie keepers handschoenen.docx">Onderzoek prijzen keepershandschoenen</a></li>
-      </ul>
+      <div class="content_container_margin">
+        <h3>Reflectie:</h3>
+          Ik ben tot conclusie gekomen dat het best lastig kan zijn om sommige dingen te onderzoeken waar niet al te
+          veel data over bekent is. En als je data hebt gevonden moet je dit altijd goed controleren zodat je geen foute data gebruikt.
+        <h3>Bewijs:</h3>
+        <ul>
+          <li><a href="https://i507258.hera.fhict.nl/Kathleen/onderzoekPlanning.docx">Post tijden TikTok & Instagram</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/research/Penalty Shooter research.docx">Penaltyshooter research</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/research/Onderzoek framework eigen project.docx">Onderzoek framework eigen project</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/research/Onderzoek prijs variantie keepers handschoenen.docx">Onderzoek prijzen keepershandschoenen</a></li>
+        </ul>
+      </div>
     </div>
     <div class="border2"></div>
     <div class="leerdoel2">
@@ -91,28 +99,32 @@
     </div>
     <div class="border1"></div>
     <div class="container_content_uitleg1">
-      <h3>Reflectie:</h3>
-        Door het te presenteren van je project krijg zeer nuttige feedback die sommige docenten of groep genoten over het
-        hoofd hebben gezien. Met deze feedback kan je weer verder mee aan de slag.
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://i507258.hera.fhict.nl/presentatie/sprint1Mac.pptx">Presentatie sprint 1</a></li>
-        <li><a href="https://i507258.hera.fhict.nl/powerpoint%20sprint%202%20mcdonalds%20v2.pptx">Presentatie sprint 2</a></li>
-      </ul>
+      <div class="content_container_margin">
+        <h3>Reflectie:</h3>
+          Door het te presenteren van je project krijg zeer nuttige feedback die sommige docenten of groep genoten over het
+          hoofd hebben gezien. Met deze feedback kan je weer verder mee aan de slag.
+        <h3>Bewijs:</h3>
+        <ul>
+          <li><a href="https://i507258.hera.fhict.nl/presentatie/sprint1Mac.pptx">Presentatie sprint 1</a></li>
+          <li><a href="https://i507258.hera.fhict.nl/powerpoint%20sprint%202%20mcdonalds%20v2.pptx">Presentatie sprint 2</a></li>
+        </ul>
+      </div>
     </div>
   </div>
   <!-- rij 6 -->
   <div class="container_row">
     <div class="container_content_uitleg2">
-      <h3>Reflectie:</h3>
-        Het hielp erg om feedback te vragen en dit weer toe te passen. Hierdoor kon voordat je verder ging werken eerst checken
-        of je zo goed te werk ging.
-      <h3>Bewijs:</h3>
-      <ul>
-        <li><a href="https://i507258.hera.fhict.nl/">Portofolio</a></li>
-        <li><a href="https://fhict.instructure.com/courses/12834/external_tools/1067">Feedpulse MacDonalds</a></li>
-        <li><a href="https://fhict.instructure.com/courses/12834/external_tools/1067">Feedpulse eigenproject</a></li>
-      </ul>
+      <div class="content_container_margin">
+        <h3>Reflectie:</h3>
+          Het hielp erg om feedback te vragen en dit weer toe te passen. Hierdoor kon voordat je verder ging werken eerst checken
+          of je zo goed te werk ging.
+        <h3>Bewijs:</h3>
+        <ul>
+          <li><a href="https://i507258.hera.fhict.nl/">Portofolio</a></li>
+          <li><a href="https://fhict.instructure.com/courses/12834/external_tools/1067">Feedpulse MacDonalds</a></li>
+          <li><a href="https://fhict.instructure.com/courses/12834/external_tools/1067">Feedpulse eigenproject</a></li>
+        </ul>
+      </div>
     </div>
     <div class="border2"></div>
     <div class="leerdoel2">
